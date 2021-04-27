6c03df5eb48f722bfca90787181d7c0ae664d46e
andreikurnosov <nixdie@yandex.ru>
2021-04-27 10:13:43 +0700

    mounting with data

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,11 @@
 import { mount } from '@vue/test-utils'
 
 const App = {
+  data() {
+    return {
+      countNum: 0
+    }
+  },
   props: {
     count: {
       type: Number
@@ -8,31 +13,40 @@ const App = {
   },
 
   template: `
-    <div v-if="count % 2 === 0">
-      Count: {{ count }}. Count is even.
+    <div v-if="countNum % 2 === 0">
+      Count: {{ countNum }}. Count is even.
     </div>
 
-    <div v-if="count % 2 !== 0">
-      Count: {{ count }}. Count is odd.
+    <div v-if="countNum % 2 !== 0">
+      Count: {{ countNum }}. Count is odd.
     </div>
   `
 }
 
-function factory(props) {
+function factory({ data }) {
   return mount(App, {
-    props
+    data() {
+      return data
+    }
   })
 }
 
 describe('App', () => {
   it('render count when even', () => {
-    const wrapper = factory({ count: 2 })
-
+    const wrapper = factory({
+      data: {
+        countNum: 2
+      }
+    })
     expect(wrapper.html()).toContain('Count: 2. Count is even.')
   })
 
   it('render count when odd', () => {
-    const wrapper = factory({ count: 1 })
+    const wrapper = factory({
+      data: {
+        countNum: 1
+      }
+    })
 
     expect(wrapper.html()).toContain('Count: 1. Count is odd.')
   })
