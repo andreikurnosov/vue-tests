209b50c01b6ae6a26ace2c5543e87e7b1b9b1ca2
andreikurnosov <nixdie@yandex.ru>
2021-04-27 09:47:20 +0700

    testing props

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,29 @@
 import { mount } from '@vue/test-utils'
 
 const App = {
+  props: {
+    count: {
+      type: Number
+    }
+  },
+
   template: `
-  <div>Hello</div>`
+    <div v-if="count % 2 === 0">
+      Count: {{ count }}. Count is even.
+    </div>
+
+    <div v-if="count % 2 !== 0">
+      Count: {{ count }}. Count is odd.
+    </div>
+  `
 }
 
 test('App', () => {
-  const wrapper = mount(App)
-  console.log(wrapper.html())
-  expect(wrapper.html()).toBe('<div>Hello</div>')
+  const wrapper = mount(App, {
+    props: {
+      count: 1
+    }
+  })
+
+  expect(wrapper.html()).toContain('Count: 1. Count is odd.')
 })
